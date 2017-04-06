55034956508cc55db6063f2e765c34c1a18ba018
thomas tsai <thomas@biotrump.com>
2017-04-07 07:30:05 +0800

    . build/envsetup.sh lunch external/iBinder/mm

--- nativeIPC/Android.mk
+++ nativeIPC/Android.mk
@@ -2,16 +2,11 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
-		server.cpp \
-		client.cpp \
-		myServer.cpp \
-		
+	client.cpp	\
+	server.cpp	\
+	myServer.cpp
 
-LOCAL_SHARED_LIBRARIES := \
-			libutils \
-			libbinder \
-			liblog \
-			libbase
+LOCAL_SHARED_LIBRARIES := libutils libbinder liblog
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/../include
 LOCAL_C_INCLUDES += $(BINDERDEMO_C_INCLUDES)
@@ -21,9 +16,8 @@ LOCAL_LDLIBS += $(BINDERDEMO_LDLIBS)
 LOCAL_STATIC_LIBRARIES := binderTC_Stub
 
 LOCAL_MODULE:= nativeIBinder
-LOCAL_CFLAGS := -Wall -Wextra -Werror -Wunused
-LOCAL_CFLAGS += -pie -fPIE
-LOCAL_LDFLAGS += -pie -fPIE
+#LOCAL_CFLAGS += -pie -fPIE
+#LOCAL_LDFLAGS += -pie -fPIE
 
-include $(BUILD_EXECUTABLE)
-include $(call first-makefiles-under,$(LOCAL_PATH))
+#include $(BUILD_EXECUTABLE)
+include $(BUILD_SHARED_LIBRARY)
