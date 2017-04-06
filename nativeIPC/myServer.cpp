913f02631b8dd81f9f417b2f07ceed71343db7f9
Thomas Tsai <thomas@life100.cc>
2017-04-07 06:36:29 +0800

    sepate service to init and message loop message loop: //Loop for waiting for request     IPCThreadState::self()->joinThreadPool();

--- nativeIPC/myServer.cpp
+++ nativeIPC/myServer.cpp
@@ -16,4 +16,30 @@ int nativeIBinderService(int argc, char *argv[])
 
     return 0;
 }
+#if 0
+thread nativeIBinderServiceInit(int argc, char *argv[])
+{
+    sp<ProcessState> proc(ProcessState::self());
+    //get service manager
+    sp<IServiceManager> sm = defaultServiceManager();
 
+    //Register a service with the SERVICE_NAME
+    sm->addService(String16(SERVICE_NAME),new demo_api::DemoAPI());
+    ALOGE("%s service is starting.....",SERVICE_NAME);
+    //Start Service
+    ProcessState::self()->startThreadPool();
+
+    return ProcessState::self();
+}
+
+int nativeIBinderServiceThread(thread)
+{
+ 
+	
+    //Loop for waiting for request
+    //IPCThreadState::self()->joinThreadPool();
+	thread->joinThreadPool();
+
+    return 0;
+}
+#endif
